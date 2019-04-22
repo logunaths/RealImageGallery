4b152f94d7a90633c9e9f539487d10d0205fb1ec
Logu Subramaniyan <logu.subramaniyan@aspiremac37.local>
2019-04-22 12:46:58 +0530

    Initial setup

---
+++ RealImageCompare/RealImageCompare.h
@@ -0,0 +1,19 @@
+//
+//  RealImageCompare.h
+//  RealImageCompare
+//
+//  Created by Logu Subramaniyan on 22/04/19.
+//  Copyright © 2019 Aspire Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealImageCompare.
+FOUNDATION_EXPORT double RealImageCompareVersionNumber;
+
+//! Project version string for RealImageCompare.
+FOUNDATION_EXPORT const unsigned char RealImageCompareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealImageCompare/PublicHeader.h>
+
+
---
+++ RealImageCompareExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealImageCompareExamples
+//
+//  Created by Logu Subramaniyan on 22/04/19.
+//  Copyright © 2019 Aspire Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
